2dff206d21f36ff20c27740f90245ef8cdbbdfd2
pablopi <pablopizarroaguilar@gmail.com>
2016-02-17 12:20:43 +0100

    Virtual Bulb answer as a normal plugin

--- hw-api.js
+++ hw-api.js
@@ -1,7 +1,12 @@
-var socket = io.connect({path: '/i/bulb-plugin'})
+/*global io power color bulb changeBulbParams*/
+var socket = io.connect()
 
 socket.on('connect', function () {
   console.log('ws:// bulb is online')
+  socket.emit('params', {
+    power: power.value,
+    color: color.value
+  })
 })
 
 socket.on('disconnect', function () {
@@ -10,24 +15,24 @@ socket.on('disconnect', function () {
 
 socket.on('set', function (params) {
   changeBulbParams(params)
+  socket.emit('params', params)
 })
 
 socket.on('on', function () {
-  if (bulbState === 'off') {
+  if (power.value === 'off') {
     bulb.onclick()
   }
 })
 
 socket.on('off', function () {
-  if (bulbState === 'on') {
+  if (power.value === 'on') {
     bulb.onclick()
   }
 })
 
 socket.on('get', function () {
   socket.emit('params', {
-    power: bulbState,
-    brightness: brightness.value,
+    power: power.value,
     color: color.value
   })
 })
